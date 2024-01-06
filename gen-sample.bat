bcfa9136a44810cde33df4f8e90f4bc2c41c48fd
Stefano Moioli <smxdev4@gmail.com>
2024-01-06 19:29:09 +0100

    add gen-sample.bat

---
+++ gen-sample.bat
@@ -0,0 +1,11 @@
+rd /s /q out
+mvn archetype:generate ^
+-DarchetypeGroupId=com.smx ^
+-DarchetypeArtifactId=ghidra-script ^
+-DarchetypeVersion=1.0-SNAPSHOT ^
+-DinteractiveMode=false ^
+-DoutputDirectory=out ^
+-DgroupId=com.smx ^
+-DartifactId=sample ^
+-Dversion=1.0-SNAPSHOT ^
+-DghidraPath=G:\ghidra_11.0_PUBLIC
