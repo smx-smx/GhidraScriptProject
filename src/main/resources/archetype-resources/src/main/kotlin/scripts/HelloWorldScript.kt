e8a09ec3a49fc31c7871454e7a99496e98125bc7
Stefano Moioli <smxdev4@gmail.com>
2024-01-11 23:21:39 +0100

    support initialization from both "InvokeBundleScript" and Ghidra Ghidra initialization requires a modified Ghidra with a JAR-based Script provider

--- src/main/resources/archetype-resources/src/main/kotlin/scripts/HelloWorldScript.kt
+++ src/main/resources/archetype-resources/src/main/kotlin/scripts/HelloWorldScript.kt
@@ -3,14 +3,17 @@ package ${groupId}.scripts
 import ghidra.app.script.GhidraScript
 import java.io.PrintWriter
 
-class HelloWorldScript(s: GhidraScript) : GhidraScript() {
-    /** inherit the script state **/
+class HelloWorldScript(s: GhidraScript?) : GhidraScript() {
+    constructor() : this(null)
     init {
-        val writer = s.javaClass.superclass.getDeclaredField("writer")
-            .also { it.isAccessible = true }
-            .get(s) as PrintWriter
+        if (s != null) {
+            /** inherit the script state **/
+            val writer = s.javaClass.superclass.getDeclaredField("writer")
+                .also { it.isAccessible = true }
+                .get(s) as PrintWriter
 
-        this.set(s.state, s.monitor, writer)
+            this.set(s.state, s.monitor, writer)
+        }
     }
 
     override fun run() {
